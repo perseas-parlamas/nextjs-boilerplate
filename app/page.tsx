4af347fda898e5ef857821ff95ca538584ee21d6
perseas-parlamas <pparlamasperseas@gmail.com>
2026-02-21 02:22:57 +0100

    Update page.tsx
    changed lines 5 and 6

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans" style={{ backgroundColor: '#5977EF' }}>
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
         <Image
           className="" //dark:invert
           src="/LogoRoad.PNG"
